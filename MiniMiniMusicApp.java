7d7765fd379bd9c809ae172f3d15c0f6882f49a2
polovni <luka.polovic11@gmail.com>
2026-01-25 21:00:47 +0100

    Command line args to experiment with sounds

--- MiniMiniMusicApp.java
+++ MiniMiniMusicApp.java
@@ -4,10 +4,16 @@ import static javax.sound.midi.ShortMessage.*;
 public class MiniMiniMusicApp {
 	public static void main(String[] args) {
 		MiniMiniMusicApp mini = new MiniMiniMusicApp();
-		mini.play();
+		if (args.length < 2) {
+			System.out.println("Don't forget the instrument and note args");
+		} else {
+			int instrument = Integer.parseInt(args[0]);
+			int note = Integer.parseInt(args[1]);
+			mini.play(instrument, note);
+		}
 	}
 
-	public void play() {
+	public void play(int instrument, int note) {
 		try {
 			Sequencer player = MidiSystem.getSequencer();
 			player.open();
@@ -17,13 +23,18 @@ public class MiniMiniMusicApp {
 			Track track = seq.createTrack();
 
 			ShortMessage msg1 = new ShortMessage();
-			msg1.setMessage(NOTE_ON, 1, 44, 100);
-			MidiEvent noteOn = new MidiEvent(msg1, 1);
-			track.add(noteOn);
+			msg1.setMessage(PROGRAM_CHANGE, 1, instrument, 0);
+			MidiEvent changeInstrument = new MidiEvent(msg1, 1);
+			track.add(changeInstrument);
 
 			ShortMessage msg2 = new ShortMessage();
-			msg2.setMessage(NOTE_OFF, 1, 44, 100);
-			MidiEvent noteOff = new MidiEvent(msg2, 16);
+			msg2.setMessage(NOTE_ON, 1, note, 100);
+			MidiEvent noteOn = new MidiEvent(msg2, 16);
+			track.add(noteOn);
+
+			ShortMessage msg3 = new ShortMessage();
+			msg3.setMessage(NOTE_OFF, 1, note, 100);
+			MidiEvent noteOff = new MidiEvent(msg3, 16);
 			track.add(noteOff);
 
 			player.setSequence(seq);
