ff8d1a48e0d9fe9ead4b04859acaff7e8173d0a6
polovni <luka.polovic11@gmail.com>
2026-01-27 20:03:40 +0100

    Pivoting to 16 instrument app

--- MiniMiniMusicApp.java
+++ MiniMiniMusicApp.java
@@ -1,47 +1,23 @@
 import javax.sound.midi.*;
+import javax.swing.*;
+import java.awt.*;
+import java.util.ArrayList;
+
 import static javax.sound.midi.ShortMessage.*;
 
 public class MiniMiniMusicApp {
+	private ArrayList<JCheckBox> checkboxList;
+	private Sequencer sequencer;
+	private Sequence sequence;
+	private Track track;
+
+	String[] instrumentNames = { "Bass drum", "Closed Hi-Hat", "Open-Hi,Hat", "Acoustic Snare",
+			"Crash Cymbal", "Hand Clap", "High Tom", "Hi Bongo", "Maracas",
+			"Whistle", "Low Conga", "Cowbell", "Vibraslap",
+			"Low-mid Tom", "High Agogo", "Open Hi Conga" };
+	int[] instruments = { 35, 42, 46, 38, 49, 39, 50, 60, 70, 72, 64, 56, 58, 47, 67, 63 };
+
 	public static void main(String[] args) {
-		MiniMiniMusicApp mini = new MiniMiniMusicApp();
-		if (args.length < 2) {
-			System.out.println("Don't forget the instrument and note args");
-		} else {
-			int instrument = Integer.parseInt(args[0]);
-			int note = Integer.parseInt(args[1]);
-			mini.play(instrument, note);
-		}
-	}
-
-	public void play(int instrument, int note) {
-		try {
-			Sequencer player = MidiSystem.getSequencer();
-			player.open();
-
-			Sequence seq = new Sequence(Sequence.PPQ, 4);
-
-			Track track = seq.createTrack();
-
-			ShortMessage msg1 = new ShortMessage();
-			msg1.setMessage(PROGRAM_CHANGE, 1, instrument, 0);
-			MidiEvent changeInstrument = new MidiEvent(msg1, 1);
-			track.add(changeInstrument);
-
-			ShortMessage msg2 = new ShortMessage();
-			msg2.setMessage(NOTE_ON, 1, note, 100);
-			MidiEvent noteOn = new MidiEvent(msg2, 16);
-			track.add(noteOn);
-
-			ShortMessage msg3 = new ShortMessage();
-			msg3.setMessage(NOTE_OFF, 1, note, 100);
-			MidiEvent noteOff = new MidiEvent(msg3, 16);
-			track.add(noteOff);
-
-			player.setSequence(seq);
-
-			player.start();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		new MiniMiniMusicApp().buildGui();
 	}
 }
